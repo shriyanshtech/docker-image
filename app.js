341f36430e9a14caab69fd30fdeee54264173fbb
Shriyansh <shreyansh.g@atechnos.com>
2021-01-06 13:54:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+express = require('express');
+morgan = require('morgan');
+path = require('path');
+methods = require('methods');
+bodyParser = require('body-parser');
+cors = require('cors');
+app.listen(process.env.PORT || 4000, ()=> {});
